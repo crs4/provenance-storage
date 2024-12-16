7f2bfe2c86bd8f6fe4b0d48aa86d7bd021552202
simleo <simleo@crs4.it>
2024-12-16 17:08:15 +0100

    fuseki: make load_crate a parametrized tool

--- fuseki/load_crate.py
+++ fuseki/load_crate.py
@@ -1,10 +1,33 @@
+import argparse
+from pathlib import Path
+
 from rdflib import Graph
 from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
 from rdflib.graph import DATASET_DEFAULT_GRAPH_ID
 
-store = SPARQLUpdateStore()
-query_endpoint = 'http://localhost:3030/ds/query'
-update_endpoint = 'http://localhost:3030/ds/update'
-store.open((query_endpoint, update_endpoint))
-graph = Graph(store, identifier=DATASET_DEFAULT_GRAPH_ID)
-graph.parse("crate1/ro-crate-metadata.json")
+
+FUSEKI_BASE_URL = "http://localhost:3030"
+FUSEKI_DATASET = "ds"
+
+
+def main(args):
+    crate_path = Path(args.crate)
+    metadata_path = crate_path / "ro-crate-metadata.json"
+    if not metadata_path.is_file():
+        raise RuntimeError(f"file {metadata_path} not found")
+    store = SPARQLUpdateStore()
+    query_endpoint = f"{args.fuseki_url}/{args.fuseki_dataset}/query"
+    update_endpoint = f"{args.fuseki_url}/{args.fuseki_dataset}/update"
+    store.open((query_endpoint, update_endpoint))
+    graph = Graph(store, identifier=DATASET_DEFAULT_GRAPH_ID)
+    graph.parse(metadata_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("crate", metavar="CRATE", help="RO-Crate")
+    parser.add_argument("-u", "--fuseki-url", metavar="STRING",
+                        help="Fuseki base url", default=FUSEKI_BASE_URL)
+    parser.add_argument("-d", "--fuseki-dataset", metavar="STRING",
+                        help="Fuseki dataset", default=FUSEKI_DATASET)
+    main(parser.parse_args())
