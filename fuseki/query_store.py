1547d63c2dbc3e39305829d1205e7d71079f3ac7
simleo <simleo@crs4.it>
2024-12-17 12:00:31 +0100

    fuseki: make query_store a parametrized tool

--- fuseki/query_store.py
+++ fuseki/query_store.py
@@ -1,21 +1,34 @@
+import argparse
+from pathlib import Path
+
 from rdflib import Graph
 from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
 
-store = SPARQLUpdateStore()
-query_endpoint = 'http://localhost:3030/ds/query'
-store.open((query_endpoint))
-graph = Graph(store)
+FUSEKI_BASE_URL = "http://localhost:3030"
+FUSEKI_DATASET = "ds"
 
-QUERY = """\
-PREFIX schema: <http://schema.org/>
 
-SELECT ?name
-WHERE {
-  ?person a schema:Person .
-  ?person schema:name ?name
-}
-"""
+def main(args):
+    query_path = Path(args.query_file)
+    if not query_path.is_file():
+        raise RuntimeError(f"file {query_path} not found")
+    with open(query_path, "rt") as f:
+        query = f.read()
+    store = SPARQLUpdateStore()
+    query_endpoint = f"{args.fuseki_url}/{args.fuseki_dataset}/query"
+    store.open((query_endpoint))
+    graph = Graph(store)
+    qres = graph.query(query)
+    for row in qres:
+        print(row)
 
-qres = graph.query(QUERY)
-for row in qres:
-    print(f"Author: {row.name}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_file", metavar="QUERY_FILE",
+                        help="SPARQL query file")
+    parser.add_argument("-u", "--fuseki-url", metavar="STRING",
+                        help="Fuseki base url", default=FUSEKI_BASE_URL)
+    parser.add_argument("-d", "--fuseki-dataset", metavar="STRING",
+                        help="Fuseki dataset", default=FUSEKI_DATASET)
+    main(parser.parse_args())
