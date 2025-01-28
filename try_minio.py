95b54d810da02b1bc4817294505a14beaee4597f
simleo <simleo@crs4.it>
2025-01-28 12:24:45 +0100

    minio script: zipped crate upload

--- try_minio.py
+++ try_minio.py
@@ -1,10 +1,21 @@
 import json
+import shutil
+
 from minio import Minio
 
+
+BUCKET = "crates"
+
 client = Minio("localhost:9000", "minio", "miniosecret", secure=False)
-client.make_bucket("bucket01")
-assert client.list_buckets() == ["bucket01"]
-client.fput_object("bucket01", "README.md", "README.md")
+if not client.bucket_exists(BUCKET):
+    client.make_bucket(BUCKET)
+print("buckets:", client.list_buckets())
+
+for crate in "crate1", "crate2":
+    shutil.make_archive(crate, "zip", crate)
+    zipped_crate = f"{crate}.zip"
+    client.fput_object(BUCKET, zipped_crate, zipped_crate)
+
 # anonymous read-only policy, see https://github.com/minio/minio-py/blob/88f4244fe89fb9f23de4f183bdf79524c712deaa/examples/set_bucket_policy.py#L27
 policy = {
     "Version": "2012-10-17",
@@ -13,16 +24,21 @@ policy = {
             "Effect": "Allow",
             "Principal": {"AWS": "*"},
             "Action": ["s3:GetBucketLocation", "s3:ListBucket"],
-            "Resource": "arn:aws:s3:::bucket01",
+            "Resource": f"arn:aws:s3:::{BUCKET}",
         },
         {
             "Effect": "Allow",
             "Principal": {"AWS": "*"},
             "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::bucket01/*",
+            "Resource": f"arn:aws:s3:::{BUCKET}/*",
         },
     ],
 }
-client.set_bucket_policy("bucket01", json.dumps(policy))
-client.remove_object("bucket01", "README.md")
-client.remove_bucket("bucket01")
+client.set_bucket_policy(BUCKET, json.dumps(policy))
+# now the bucket's objects are accessible via http as:
+#   http://localhost:9000/BUCKET/OBJECT
+# e.g. curl -o crate1.zip http://localhost:9000/crates/crate1.zip
+
+# client.remove_object(BUCKET, "crate1.zip")
+# client.remove_object(BUCKET, "crate2.zip")
+# client.remove_bucket(BUCKET)
