fc66ac78b8fc0ba887b08756c3a79213c708722d
simleo <simleo@crs4.it>
2025-02-20 16:15:47 +0100

    refactor as a nested cli tool

--- src/provstore/load.py
+++ src/provstore/load.py
@@ -1,4 +1,3 @@
-import argparse
 import json
 import shutil
 import tempfile
@@ -47,7 +46,7 @@ def upload_crate_to_minio(crate_path, crate_name=None, bucket=MINIO_BUCKET):
         print(f'created bucket "{bucket}"')
         client.set_bucket_policy(MINIO_BUCKET, json.dumps(MINIO_BUCKET_POLICY))
     tmp_dir = Path(tempfile.mkdtemp(prefix="load_crate_"))
-    archive = shutil.make_archive(tmp_dir/crate_name, "zip", crate_path)
+    archive = shutil.make_archive(tmp_dir / crate_name, "zip", crate_path)
     zipped_crate_name = f"{crate_name}.zip"
     client.fput_object(bucket, zipped_crate_name, archive)
     crate_url = f"http://{MINIO_STORE}/{MINIO_BUCKET}/{zipped_crate_name}"
@@ -55,27 +54,20 @@ def upload_crate_to_minio(crate_path, crate_name=None, bucket=MINIO_BUCKET):
     return crate_url
 
 
-def main(args):
-    crate_path = Path(args.crate)
+def load_crate_metadata(crate_path, fuseki_url=None, fuseki_dataset=None):
     crate_url = upload_crate_to_minio(crate_path)
+    if not fuseki_url:
+        fuseki_url = FUSEKI_BASE_URL
+    if not fuseki_dataset:
+        fuseki_dataset = FUSEKI_DATASET
     metadata_path = crate_path / "ro-crate-metadata.json"
     if not metadata_path.is_file():
         raise RuntimeError(f"file {metadata_path} not found")
     store = SPARQLUpdateStore()
-    query_endpoint = f"{args.fuseki_url}/{args.fuseki_dataset}/sparql"
-    update_endpoint = f"{args.fuseki_url}/{args.fuseki_dataset}/update"
+    query_endpoint = f"{fuseki_url}/{fuseki_dataset}/sparql"
+    update_endpoint = f"{fuseki_url}/{fuseki_dataset}/update"
     store.open((query_endpoint, update_endpoint))
     graph = Graph(store, identifier=URIRef(crate_url))
     loc = arcp.arcp_location(crate_url)
     print("ARCP location:", loc)
     graph.parse(metadata_path, publicID=loc)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("crate", metavar="CRATE", help="RO-Crate")
-    parser.add_argument("-u", "--fuseki-url", metavar="STRING",
-                        help="Fuseki base url", default=FUSEKI_BASE_URL)
-    parser.add_argument("-d", "--fuseki-dataset", metavar="STRING",
-                        help="Fuseki dataset", default=FUSEKI_DATASET)
-    main(parser.parse_args())
