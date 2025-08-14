dde453a7195a9dcd885b36dfbf90562d987667a1
nicologiacomini <nicolo.giacomini@bsc.es>
2025-08-14 23:30:56 +0200

    add function to get the file by uri

--- src/provstor_api/routes/get.py
+++ src/provstor_api/routes/get.py
@@ -52,3 +52,48 @@ async def get_crate(rde_id: str, outdir: str):
         logging.error(f"Error downloading crate: {e}")
         raise HTTPException(status_code=500, detail=f"Download failed: {str(e)}")
 
+
+@router.get("/file/")
+async def get_file(file_uri: str, outdir: str):
+    try:
+        outdir = Path(outdir)
+
+        if not outdir.is_absolute():
+            raise HTTPException(status_code=400, detail="The destination directory must be an absolute path")
+
+        if file_uri.startswith("http"):
+            out_path = outdir / file_uri.rsplit("/", 1)[-1]
+            with urlopen(file_uri) as response, out_path.open("wb") as f:
+                shutil.copyfileobj(response, f)
+            return FileResponse(
+                path=out_path,
+                filename=out_path.name,
+                media_type='application/octet-stream',
+                headers={"download_path": str(out_path)}
+            )
+        elif not file_uri.startswith("arcp"):
+            raise ValueError(f"{file_uri}: unsupported protocol")
+        res = urlsplit(file_uri)
+        rde_id = f"{res.scheme}://{res.netloc}/"
+        zip_dir = Path(tempfile.mkdtemp())
+        zip_path = await get_crate(rde_id, outdir=zip_dir)
+        zip_member = res.path.lstrip("/")
+        logging.info("extracting: %s", zip_member)
+        with zipfile.ZipFile(zip_path.path, "r") as zipf:
+            out_path = zipf.extract(zip_member, path=outdir)
+        shutil.rmtree(zip_dir)
+        download_file = zip_member.rsplit("/", 1)[-1]
+
+        return FileResponse(
+            path=out_path,
+            filename=download_file,
+            media_type='application/zip',
+            headers={"download_path": str(out_path)}
+        )
+
+    except HTTPException:
+        raise
+    except Exception as e:
+        logging.error(f"Error downloading file: {e}")
+        raise HTTPException(status_code=500, detail=f"Download failed: {str(e)}")
+
