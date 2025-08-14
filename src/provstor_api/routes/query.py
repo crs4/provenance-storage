25234a6c4d28b208b864b3c1532c890fb69ee658
nicologiacomini <nicolo.giacomini@bsc.es>
2025-08-14 23:27:46 +0200

    add function to get the list of graphs

---
+++ src/provstor_api/routes/query.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, UploadFile, HTTPException
+from provstor_api.utils.query import run_query
+from provstor_api.utils.queries import GRAPHS_QUERY, RDE_GRAPH_QUERY
+import logging
+
+router = APIRouter()
+
+
+@router.get("/list-graphs/")
+def list_graphs():
+    try:
+        query_res = run_query(GRAPHS_QUERY)
+        output = []
+        for (i, item) in zip(range(len(query_res)), query_res):
+            output.append(item[0])
+        return {"result": output}
+    except Exception as e:
+        logging.error(f"Error fetching graphs: {e}")
+        raise HTTPException(status_code=502, detail=f"Failed to fetch graphs: {str(e)}")
+
+Exception(status_code=502, detail=f"Failed to fetch RDE IDs: {str(e)}")
