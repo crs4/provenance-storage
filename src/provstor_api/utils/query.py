8edbe28fea7655d494e6f7d01abcf82e6c28be2c
nicologiacomini <nicolo.giacomini@bsc.es>
2025-08-14 23:23:20 +0200

    add function used to run the query

---
+++ src/provstor_api/utils/query.py
@@ -0,0 +1,18 @@
+from rdflib.term import URIRef
+from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
+
+from provstor_api.config import (
+    FUSEKI_BASE_URL, FUSEKI_DATASET,
+    MINIO_STORE, MINIO_BUCKET
+)
+
+def run_query(query, graph_id=None):
+    store = SPARQLUpdateStore()
+    query_endpoint = f"{FUSEKI_BASE_URL}/{FUSEKI_DATASET}/sparql"
+    store.open(query_endpoint)
+    if graph_id:
+        if not graph_id.startswith("http://"):
+            graph_id = f"http://{MINIO_STORE}/{MINIO_BUCKET}/{graph_id}.zip"
+        graph_id = URIRef(graph_id)
+    qres = store.query(query, queryGraph=graph_id)
+    return qres
