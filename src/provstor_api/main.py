a66c89d406655730c1057eb7428705830d561d2e
nicologiacomini <nicolo.giacomini@bsc.es>
2025-08-14 23:11:27 +0200

    init api directory

---
+++ src/provstor_api/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from routes import upload, query, get
+import os
+import uvicorn
+import logging
+
+logging.getLogger().setLevel(logging.INFO)
+
+app = FastAPI(title="Provenance Storage API", version="1.0")
+
+app.include_router(query.router, prefix="/query", tags=["Query"])
+app.include_router(get.router, prefix="/get", tags=["Get"])
+# app.include_router(upload.router, prefix="/upload", tags=["Upload"])
+
+
+@app.get("/status/")
+def check_status():
+    return {"status": "ok"}
+
+@app.get("/favicon.ico", include_in_schema=False)
+def favicon():
+    icon_file = os.path.join(os.path.dirname(__file__), "static", "favicon.ico")
+    return FileResponse(icon_file)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
