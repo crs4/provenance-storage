bbdc1fdc4a11920e9d531f8491decb54943eb41a
nicologiacomini <nicolo.giacomini@bsc.es>
2025-08-15 10:00:55 +0200

    fix return get_workflow function

--- src/provstor_api/routes/get.py
+++ src/provstor_api/routes/get.py
@@ -124,9 +124,8 @@ def get_graphs_for_result(result_id: str):
 def get_workflow(graph_id: str):
     try:
         query_res = run_query(WORKFLOW_QUERY, graph_id=graph_id)
-        output = next(iter(query_res), None)
-        result = output[0] if output is not None else Literal('')
-        return {"result": result}
+        output = [str(_[0]) for _ in query_res]
+        return {"result": output}
     except Exception as e:
         logging.error(f"Error retrieving graph from input file: {e}")
         raise HTTPException(status_code=502, detail=f"Failed to retrieve graph from input file: {str(e)}")
