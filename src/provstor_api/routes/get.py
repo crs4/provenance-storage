84f351d43fae92b6b70e07547657fdbc9f2e01bb
nicologiacomini <nicolo.giacomini@bsc.es>
2025-08-16 01:56:56 +0200

    add function to get the results by the action id

--- src/provstor_api/routes/get.py
+++ src/provstor_api/routes/get.py
@@ -5,7 +5,7 @@ from provstor_api.utils.queries import (
     CRATE_URL_QUERY, GRAPH_ID_FOR_FILE_QUERY,
     GRAPH_ID_FOR_RESULT_QUERY, WORKFLOW_QUERY, WFRUN_RESULTS_QUERY,
     WFRUN_OBJECTS_QUERY, OBJECTS_FOR_RESULT_QUERY, ACTIONS_FOR_RESULT_QUERY,
-    OBJECTS_FOR_ACTION_QUERY
+    OBJECTS_FOR_ACTION_QUERY, RESULTS_FOR_ACTION_QUERY
 )
 import logging
 from pathlib import Path
@@ -181,6 +181,17 @@ def get_actions_for_result(result_id: str):
 def get_objects_for_action(action_id: str):
     try:
         query_res = run_query(OBJECTS_FOR_ACTION_QUERY % {"action": action_id})
+        output = [str(_[0]) for _ in query_res]
+        return {"result": output}
+    except Exception as e:
+        logging.error(f"Error retrieving graph from input file: {e}")
+        raise HTTPException(status_code=502, detail=f"Failed to retrieve graph from input file: {str(e)}")
+
+
+@router.get("/results-for-action/")
+def get_results_for_action(action_id: str):
+    try:
+        query_res = run_query(RESULTS_FOR_ACTION_QUERY % {"action": action_id})
         output = [str(_[0]) for _ in query_res]
         return {"result": output}
     except Exception as e:
