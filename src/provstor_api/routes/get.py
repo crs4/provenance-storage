ccf3bb94139730b1253d1f65695163e6f3d11393
nicologiacomini <nicolo.giacomini@bsc.es>
2025-08-16 01:20:27 +0200

    add function to get the objects by the result id

--- src/provstor_api/routes/get.py
+++ src/provstor_api/routes/get.py
@@ -4,7 +4,7 @@ from provstor_api.utils.query import run_query
 from provstor_api.utils.queries import (
     CRATE_URL_QUERY, GRAPH_ID_FOR_FILE_QUERY,
     GRAPH_ID_FOR_RESULT_QUERY, WORKFLOW_QUERY, WFRUN_RESULTS_QUERY,
-    WFRUN_OBJECTS_QUERY
+    WFRUN_OBJECTS_QUERY, OBJECTS_FOR_RESULT_QUERY
 )
 import logging
 from pathlib import Path
@@ -153,3 +153,13 @@ def get_run_objects(graph_id: str):
         logging.error(f"Error retrieving graph from input file: {e}")
         raise HTTPException(status_code=502, detail=f"Failed to retrieve graph from input file: {str(e)}")
 
+
+@router.get("/objects-for-result/")
+def get_objects_for_result(result_id: str):
+    try:
+        query_res = run_query(OBJECTS_FOR_RESULT_QUERY % result_id)
+        output = [str(_[0]) for _ in query_res]
+        return {"result": output}
+    except Exception as e:
+        logging.error(f"Error retrieving graph from input file: {e}")
+        raise HTTPException(status_code=502, detail=f"Failed to retrieve graph from input file: {str(e)}")
