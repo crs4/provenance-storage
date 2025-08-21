785f9a8d6e5e36a3dba7343d3bb11b6129b5e327
nicologiacomini <nicolo.giacomini@bsc.es>
2025-08-21 20:36:57 +0200

    refactor: simplify fetch functions to return lists directly

--- src/provstor_api/utils/get_utils.py
+++ src/provstor_api/utils/get_utils.py
@@ -8,15 +8,12 @@ from utils.query import run_query
 
 def fetch_actions_for_result(result_id):
     query_res = run_query(ACTIONS_FOR_RESULT_QUERY % result_id)
-    output = [str(_[0]) for _ in query_res]
-    return {"result": output}
+    return [str(_[0]) for _ in query_res]
 
 def fetch_objects_for_action(action_id):
     query_res = run_query(OBJECTS_FOR_ACTION_QUERY % {"action": action_id})
-    output = [str(_[0]) for _ in query_res]
-    return {"result": output}
+    return [str(_[0]) for _ in query_res]
 
 def fetch_results_for_action(action_id):
     query_res = run_query(RESULTS_FOR_ACTION_QUERY % {"action": action_id})
-    output = [str(_[0]) for _ in query_res]
-    return {"result": output}
+    return [str(_[0]) for _ in query_res]
