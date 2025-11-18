13d4187ebfa78b03aae669c142ad8ce8265dccad
simleo <simleo@crs4.it>
2025-11-18 16:54:08 +0100

    fix test_cli_backtrack

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -389,21 +389,23 @@ def test_cli_backtrack(crate_map):
     assert set(items[1][2]) >= {
         "file:///path/to/FOOBAR123.deepvariant.ann.vcf.gz"
     }
-    assert items[2][0] == f"{provcrate1_rde_id}#12204f1e-758f-46e7-bad7-162768de3a5d"
-    assert set(items[2][1]) >= {
+    # ordering of the last two rows is undefined (2 actions for the same result)
+    i, j = (2, 3) if "publish" in items[3][0] else (3, 2)
+    assert items[i][0] == f"{provcrate1_rde_id}#12204f1e-758f-46e7-bad7-162768de3a5d"
+    assert set(items[i][1]) >= {
         "file:///path/to/FOOBAR123_1.fastq.gz",
         "file:///path/to/FOOBAR123_2.fastq.gz",
         "file:///path/to/pipeline_info/software_versions.yml",
         "http://example.com/fooconfig.yml",
         f"{provcrate1_rde_id}sample.csv",
     }
-    assert set(items[2][2]) >= {
+    assert set(items[i][2]) >= {
         "file:///path/to/FOOBAR123.deepvariant.vcf.gz.tbi",
         "file:///path/to/FOOBAR123.deepvariant.vcf.gz"
     }
-    assert items[3][0] == f"{provcrate1_rde_id}#publish/13fc2459df3405bf049e575f063aef3d/FOOBAR123.deepvariant.vcf.gz"
-    assert items[3][1] == []  # object is not a file or directory
-    assert set(items[3][2]) >= {
+    assert items[j][0] == f"{provcrate1_rde_id}#publish/13fc2459df3405bf049e575f063aef3d/FOOBAR123.deepvariant.vcf.gz"
+    assert items[j][1] == []  # object is not a file or directory
+    assert set(items[j][2]) >= {
         "file:///path/to/FOOBAR123.deepvariant.vcf.gz",
     }
 
